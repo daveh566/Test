3c94709dcc9f2669418886a73a1cac78dd5493bb
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-08-21 16:37:49 +0300

    Update __init__.py

--- Test/__init__.py
+++ Test/__init__.py
@@ -6,7 +6,7 @@ from telethon import TelegramClient
 from decouple import config
 import logging
 import time
-from Test.config import APP_ID, API_HASH, BOT_TOKEN
+from Test.config import API_ID, API_HASH, BOT_TOKEN
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -14,9 +14,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 """
 #Normal Usage
 
-APP_ID = config("APP_ID", default=None, cast=int)
+API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 """
 
-app = TelegramClient('app', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+app = TelegramClient('app', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
